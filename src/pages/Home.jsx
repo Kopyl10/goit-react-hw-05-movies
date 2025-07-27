668eb6ee43913790554291794b697ddf46fb0a1b
Kopyl10 <kopyl10@gmail.com>
2025-07-27 10:26:52 -0700

    change new

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,10 @@
 import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
 import css from './Home.module.css';
 import MovieList from './MovieList';
 import { fetchTrendingMovies } from '../services/api';
 
 export default function Home() {
   const [movies, setMovies] = useState([]);
-  const location = useLocation();
 
   useEffect(() => {
     fetchTrendingMovies()
